4828e9848d5ed305ab5657ef1d148e0bb89d1734
Denis Brevnov <denisbrevnov2006@gmail.com>
2026-03-24 18:32:26 +0500

    3 punkt

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  let a = {items : [{
+      "id": 1,
+      "name": "name"
+    },
+      {
+        "id": 1,
+        "name": "Ирина"
+      },
+      {
+        "id": 1,
+        "name": "Денис"
+      }]};
+  res.send(a);
 });
 
 module.exports = router;
