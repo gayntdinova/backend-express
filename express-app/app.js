f376d0e6101d53518da1b5dc9e29a7ca44e717da
Irina Gayntdinova <gajntdinovairina@gmail.com>
2026-03-24 19:01:13 +0500

    6

--- express-app/app.js
+++ express-app/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -14,7 +15,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use((req, res, next) => {
+  if (req.query.auth === "true") {
+    next();
+  } else {
+    res.status(401).send('Not authorized');
+  }});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
